29464b71313294a0bd074e1a9a0e6c2635310bfc
Tobias Xhonneux <tobiasxhonneux@hotmail.com>
2021-05-12 13:53:26 +0200

    Initial Code

---
+++ USBDeviceFinder/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace USBDeviceFinder
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        private const int DBT_DEVTYP_VOLUME = 0x00000002;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    // See values here: https://docs.microsoft.com/en-us/windows/win32/devio/wm-devicechange
+                    switch ((int)m.WParam)
+                    {
+                        case DBT_DEVICEARRIVAL:
+                            listBox1.Items.Add("New Device Arrived");
+
+                            int devType = Marshal.ReadInt32(m.LParam, 4);
+                            if (devType == DBT_DEVTYP_VOLUME)
+                            {
+                                listBox1.Items.Add("devType is " + devType);
+                            }
+
+                            break;
+
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            listBox1.Items.Add("Device Removed");
+                            break;
+
+                        default:
+                            listBox1.Items.Add("m.WParam is " + m.WParam);
+                            break;
+                    }
+                    break;
+            }
+
+        }
+
+    }
+}
